f7d35462c340d1d13f728d902dadd972815232b2
kuba <jk.ziolkowski@gmail.com>
2025-03-14 15:03:12 +0100

    ssl

--- src/server/ssl.js
+++ src/server/ssl.js
@@ -5,6 +5,9 @@ var settings = require('./settings'),
 
 forge.options.usePureJavaScript = true
 
+const fs = require('fs');
+const path = require('path');
+
 // Generate a random Serial number for the certificate
 // matth-c3 @ https://github.com/digitalbazaar/forge/issues/673#issuecomment-500900852
 function randomSerialNumber() {
@@ -17,6 +20,40 @@ function randomSerialNumber() {
 
     mostSiginficativeHexAsInt -= 8
     return mostSiginficativeHexAsInt.toString() + hexString.substring(1)
+}
+
+function loadCertificateFromFile() {
+    const certDir = path.join(__dirname, '..', '..', 'certs');
+    const certPath = path.join(certDir, 'certificate.crt');
+    const privateKeyPath = path.join(certDir, 'private.key');
+
+    if (fs.existsSync(certPath) && fs.existsSync(privateKeyPath)) {
+        try {
+            const cert = fs.readFileSync(certPath, 'utf8');
+            const key = fs.readFileSync(privateKeyPath, 'utf8');
+
+            // Verify the certificate and key
+            const certObj = pki.certificateFromPem(cert);
+            const keyObj = pki.privateKeyFromPem(key);
+
+            // Check if the certificate is valid
+            const now = new Date();
+            if (now < certObj.validity.notBefore || now > certObj.validity.notAfter) {
+                console.log('(INFO) Certificate is expierd, creating a new one.');
+                return createCertificate();
+            }
+
+            console.log('(INFO) If it crashes now the certificate is invalid ');
+            return settings.write('ssl-certificate', { cert, key });
+        } catch (error) {
+            console.error('(ERROR) Failed to load certificate from files:', error);
+            
+            return createCertificate();
+        }
+    } else {
+        console.log('(INFO) Certificate or key file not found. Creating a new self-signed certificate.');
+        return createCertificate();
+    }
 }
 
 function createCertificate() {
@@ -45,11 +82,21 @@ function createCertificate() {
         cert: pki.certificateToPem(cert)
     })
 
+    const certDir = path.join(__dirname, '..', '..', 'certs');
+    if (!fs.existsSync(certDir)){
+        fs.mkdirSync(certDir, { recursive: true });
+    }
+
+    fs.writeFileSync(path.join(certDir, 'certificate.crt'), pki.certificateToPem(cert));
+    fs.writeFileSync(path.join(certDir, 'private.key'), pki.privateKeyToPem(keys.privateKey));
+    fs.writeFileSync(path.join(certDir, 'public.key'), pki.publicKeyToPem(keys.publicKey));
+
 }
 
 if (!certificate) {
+    loadCertificateFromFile();
 
-    createCertificate()
+    // createCertificate()
 
 } else {
 
